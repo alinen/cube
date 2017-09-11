1154634cf3d888748f2e731a0e274c5877224988
U-Hecubus\alinen <nenila@gmail.com>
2017-09-11 18:35:21 -0400

    look at script for tracking the cubie to be solved

--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -6,22 +6,37 @@ public class LookAt : MonoBehaviour
 {
     public Transform target;
     public float radius = 4.0f;
-    public float rate = 0.001f;
+    public float rate = 5.0f;
+
+    private Vector3 _startPos;
+    private float _time;
 
 	void Start ()
     {
+        transform.position = transform.position.normalized * radius;
         SetTarget(target);
 	}
 	
     public void SetTarget(Transform t)
     {
         target = t;
+        _startPos = transform.position;
+        _time = 0;
     }
 
 	void Update ()
     {
         Vector3 targetPos = target.position.normalized * radius;
-        transform.position = Vector3.Lerp(transform.position, targetPos, Mathf.Pow(rate, Time.deltaTime));
-        transform.rotation = Quaternion.LookRotation(-targetPos);
+        _time += Time.deltaTime;
+        if (_time < rate)
+        {
+            float frac = _time / rate;
+            transform.position = Vector3.Slerp(_startPos, targetPos, frac);
+        }
+        else
+        {
+            transform.position = targetPos;
+        }
+        transform.rotation = Quaternion.LookRotation(-transform.position);
 	}
 }
